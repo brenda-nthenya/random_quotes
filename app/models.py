c720ec2284e337dd09016c07eab9062d237ed269
Brenda <bee@bee.com>
2022-05-16 16:05:00 +0300

    Adding the session and commit functions to user amd blog classes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+from sys import settrace
 from . import db
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Quotes:
     '''Shows how the API will be consumed'''
@@ -13,6 +14,27 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the passowrd attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure,password)
+
 
 
     def __repr__(self):
@@ -26,3 +48,10 @@ class Blog(db.Model):
     post = db.Column(db.Text())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
+    def __repr__(self):
+        return f'Pitch {self.post}'
