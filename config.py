33d85e099553a6f2dd2c2fcee35657256f76c796
Brenda <bee@bee.com>
2022-05-17 15:16:16 +0300

    Adds mail configurations to parent class

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+import os
 class Config:
-    SECRET_KEY = 'ForWHateverReason'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bee:QWERTY098@localhost/blogs'
-    pass
+
+    # Mail Configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     pass
