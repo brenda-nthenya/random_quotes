41cfb86942c9b768405669d78a6bf2cd14a4ed3c
Brenda <bee@bee.com>
2022-05-17 15:09:09 +0300

    Adds login

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,23 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    config_options[config_name].init_app(app)
+   
 
     # Initializing flask extensions
     bootstrap.init_app(app)
