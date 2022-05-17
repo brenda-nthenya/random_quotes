fa707949ff3d65d9363d1812f5e86aa9b2263d74
Brenda <bee@bee.com>
2022-05-17 16:02:54 +0300

    Adds the comments module

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,7 +14,7 @@ class Quotes:
         self.author = author
         self.quote = quote
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -28,7 +29,7 @@ class User(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    def delete(self):
+    def delete_user(self):
         db.session.delete(self)
         db.session.commit()
 
@@ -57,15 +58,35 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     category = db.Column(db.String(255), index=True)
     time = db.Column(db.DateTime, default = datetime.utcnow)
+    comment = db.relationship('Comments', backref='user', lazy='dynamic')
 
     def save_blog(self):
         db.session.add(self)
         db.session.commit()
 
-    def delete(self):
+    def delete_blog(self):
         db.session.delete(self)
         db.session.commit()
 
         
     def __repr__(self):
-        return f'Pitch {self.post}'
+        return f'Blog {self.post}'
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.name}'
