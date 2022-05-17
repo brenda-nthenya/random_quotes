a32c11b81171a8bde5011738073425fb0f5625ed
Brenda <bee@bee.com>
2022-05-17 15:18:55 +0300

    Adds MigrateCommand

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server,Shell
 from app.models import *
+from flask_migrate import Migrate, MigrateCommand
 
 app =create_app('development')
 
@@ -9,9 +10,10 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User )
+    return dict(app=app, db=db, User=User)
 
-
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
