1c15dbb4e69865023583d3553ea6d8034c60e894
Brenda <bee@bee.com>
2022-05-19 01:45:24 +0300

    Corrects the logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,7 @@ from app.email import mail_message
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user = User.query.filter_by(emails = login_form.email.data).first()
+        user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
@@ -18,13 +18,6 @@ def login():
         flash('Invalid username or Password')
 
     return render_template('auth/login.html',login_form = login_form)
-
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("main.index"))
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -35,8 +28,13 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message('Inspire and be inspired.Welcome',"email/welcome_user",user.email, user=user)
-
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
