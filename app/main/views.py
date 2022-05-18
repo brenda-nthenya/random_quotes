c295cd2dcc5d7b250c593b5db24d35876f4e1a2f
Brenda <bee@bee.com>
2022-05-18 12:28:03 +0300

    Adds like and dislike view routes

--- app/main/views.py
+++ app/main/views.py
@@ -105,3 +105,34 @@ def blog(blog_id):
     title = 'Blogs'
     return render_template('blog/blog.html', title = title, blog = blog, comments = comments, heading = heading)
 
+@main.route('/like/<int:id>',methods = ['POST','GET'])
+@login_required
+def like(id):
+    get_blogs = Upvote.get_upvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for blog in get_blogs:
+        to_str = f'{blog}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Upvote(user = current_user, blog_id=id)
+    new_vote.save()
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    blog = Downvote.get_downvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for b in blog:
+        to_str = f'{b}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_downvote = Downvote(user = current_user, blog_id=id)
+    new_downvote.save()
+    return redirect(url_for('main.index',id = id))
